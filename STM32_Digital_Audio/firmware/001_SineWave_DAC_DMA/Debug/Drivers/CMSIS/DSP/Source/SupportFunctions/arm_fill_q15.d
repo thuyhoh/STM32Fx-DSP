622983d1f62df1ba3c87809b9c33aaa08955bb53
Thuyh0h <trongthuynl2004@gmail.com>
2025-12-20 22:12:28 +0700

    DAC in STM32F407

---
+++ STM32_Digital_Audio/firmware/001_SineWave_DAC_DMA/Debug/Drivers/CMSIS/DSP/Source/SupportFunctions/arm_fill_q15.d
@@ -0,0 +1,8 @@
+Drivers/CMSIS/DSP/Source/SupportFunctions/arm_fill_q15.o: \
+ ../Drivers/CMSIS/DSP/Source/SupportFunctions/arm_fill_q15.c \
+ D:/Embedded_MCU/STM32/STM32F40x/STM32-DSP/STM32_Digital_Audio/firmware/001_SineWave_DAC_DMA/Drivers/CMSIS/DSP/Include/arm_math.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h
+D:/Embedded_MCU/STM32/STM32F40x/STM32-DSP/STM32_Digital_Audio/firmware/001_SineWave_DAC_DMA/Drivers/CMSIS/DSP/Include/arm_math.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
---
+++ STM32_Digital_Audio/firmware/002_SineWave_DAC_FreeRTOS/Debug/Drivers/CMSIS/DSP/Source/SupportFunctions/arm_float_to_q7.d
@@ -0,0 +1,8 @@
+Drivers/CMSIS/DSP/Source/SupportFunctions/arm_float_to_q7.o: \
+ ../Drivers/CMSIS/DSP/Source/SupportFunctions/arm_float_to_q7.c \
+ D:/Embedded_MCU/STM32/STM32F40x/STM32-DSP/STM32_Digital_Audio/firmware/002_SineWave_DAC_FreeRTOS/Drivers/CMSIS/DSP/Include/arm_math.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h
+D:/Embedded_MCU/STM32/STM32F40x/STM32-DSP/STM32_Digital_Audio/firmware/002_SineWave_DAC_FreeRTOS/Drivers/CMSIS/DSP/Include/arm_math.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
